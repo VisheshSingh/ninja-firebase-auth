5cf0358a37c0e6a910d4d34d57d0a6a78decdde0
Vishesh <visheshsingh16@gmail.com>
2019-01-11 20:37:32 -0500

    Creating a user

---
+++ auth.js
@@ -0,0 +1,18 @@
+// signup
+
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    // Get email and password
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+
+    // create account
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred);
+        const modal = document.querySelector('#modal-signup');
+        M.Modal.getInstance(modal).close();
+        signupForm.reset();
+    })
+})
