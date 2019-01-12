7ed584b8d9982405913c45045214de0eea1fef64
Vishesh <visheshsingh16@gmail.com>
2019-01-11 20:23:41 -0500

    HTML Template and Firebase locally setup

---
+++ index.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    const modals = document.querySelectorAll('.modal');
+    M.Modal.init(modals);
+
+    const items = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(items);
+})
+
