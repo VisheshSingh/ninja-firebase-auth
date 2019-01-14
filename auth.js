9c67080d170dcff59f1c94edccd8eff4ff11f8b0
Vishesh <visheshsingh16@gmail.com>
2019-01-14 14:01:31 -0500

    Adding Guides

--- auth.js
+++ auth.js
@@ -12,6 +12,22 @@ auth.onAuthStateChanged(user => {
         snapshotGuides([]);
     }
 })
+// create new guide
+const createForm = document.querySelector('#create-form');
+createForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    // add to db
+    db.collection('guides').add({
+        title: createForm['title'].value,
+        content: createForm['content'].value,
+    }).then(() => {
+        const modal = document.querySelector('#modal-create');
+        M.Modal.getInstance(modal).close();
+        createForm.reset();
+    }).catch(err => {
+        console.log(err.message);
+    })
+})
 
 // signup
 const signupForm = document.querySelector('#signup-form');
